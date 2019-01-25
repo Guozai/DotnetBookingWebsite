e66131565ec188aab01251dd52b3ed64590e469c
Yiping Guo <yiping_guo@outlook.com>
2019-01-25 15:34:04 +1100

    Add Scaffolder Identity files to project

---
+++ ASRSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ASRSystem.Data;
+using ASRSystem.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASRSystem.Areas.Identity.IdentityHostingStartup))]
+namespace ASRSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
