cc5ea30006411136ba7a87ca18beef6e4bdffa77
Yiping Guo <yiping_guo@outlook.com>
2019-01-28 18:47:14 +1100

    Student booking version 1 working

--- ASRSystem/Controllers/StudentController.cs
+++ ASRSystem/Controllers/StudentController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Asr.Data;
 
 namespace ASRSystem.Controllers
@@ -7,9 +10,55 @@ namespace ASRSystem.Controllers
     [Authorize(Roles = Constants.StudentRole)]
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        private readonly AsrContext _context;
+
+        public StudentController(AsrContext context) => _context = context;
+
+        // GET: Slots
+        public async Task<IActionResult> Index() => View(await _context.Slot.ToListAsync());
+
+        // GET: Movies/Edit
+        public async Task<IActionResult> Edit(string RoomID, DateTime? StartTime)
         {
-            return View();
+            if (StartTime == null)
+                return NotFound();
+
+            var slot = await _context.Slot.FirstOrDefaultAsync(x => x.RoomID == RoomID && x.StartTime == StartTime);
+            if (slot == null)
+                return NotFound();
+
+            return View(slot);
+        }
+
+        // POST: Movies/Edit
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string RoomID, DateTime StartTime)
+        {
+            if (StartTime == null)
+                return NotFound();
+
+            var slot = await _context.Slot.FirstOrDefaultAsync(x => x.RoomID == RoomID && x.StartTime == StartTime);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(slot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (slot == null)
+                        return NotFound();
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(slot);
         }
     }
 }
