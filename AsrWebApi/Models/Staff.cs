cda0f7935127ab89c527f2def38f479a1a751522
Yiping Guo <yiping_guo@outlook.com>
2019-01-31 22:05:40 +1100

    Create saparate WebApi project and make Rooms working

---
+++ AsrWebApi/Models/Staff.cs
@@ -0,0 +1,19 @@
+﻿// Model copied from Matthew Bolger's model of Week7 example code
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsrWebApi.Models
+{
+    public class Staff
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string StaffID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ AsrWebApi/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿// Model copied from Matthew Bolger's model of Week7 example code
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsrWebApi.Models
+{
+    public class Student
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string StudentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
