f543d5a924addfd66e92a3d1f10cb0c998699e28
Yiping Guo <yiping_guo@outlook.com>
2019-01-31 18:44:44 +1100

    Change minor error of RoomAvailability and add 2 webapi controllers

---
+++ ASRSystem/Controllers/RoomsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Asr.Data;
+using Asr.Models;
+
+namespace Asr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly AsrContext _context;
+
+        public RoomsController(AsrContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rooms
+        [HttpGet]
+        public IEnumerable<Room> GetRoom()
+        {
+            return _context.Room;
+        }
+
+        // GET: api/Rooms/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoom([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var room = await _context.Room.FindAsync(id);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(room);
+        }
+
+        // PUT: api/Rooms/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRoom([FromRoute] string id, [FromBody] Room room)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != room.RoomID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(room).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rooms
+        [HttpPost]
+        public async Task<IActionResult> PostRoom([FromBody] Room room)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Room.Add(room);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RoomExists(room.RoomID))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRoom", new { id = room.RoomID }, room);
+        }
+
+        // DELETE: api/Rooms/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRoom([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var room = await _context.Room.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            _context.Room.Remove(room);
+            await _context.SaveChangesAsync();
+
+            return Ok(room);
+        }
+
+        private bool RoomExists(string id)
+        {
+            return _context.Room.Any(e => e.RoomID == id);
+        }
+    }
+}
