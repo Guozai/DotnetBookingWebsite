4d2b3511b51b41f3478caba48db3ee72659362d2
Yiping Guo <yiping_guo@outlook.com>
2019-02-02 15:26:50 +1100

    Deleted Error Message for Developers

--- ASRSystem/Startup.cs
+++ ASRSystem/Startup.cs
@@ -49,16 +49,8 @@ namespace Asr
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if(env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
