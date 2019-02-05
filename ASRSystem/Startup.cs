02c6cdbc2217d7ddd99960303b1945bd30c49729
Yiping Guo <yiping_guo@outlook.com>
2019-02-05 17:39:59 +1100

    Add Custom Error page working

--- ASRSystem/Startup.cs
+++ ASRSystem/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Asr.Data;
 using Asr.Models;
 
@@ -47,8 +48,11 @@ namespace Asr
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -59,6 +63,9 @@ namespace Asr
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
+
+            //set up a middleware to handle the request in the pipeline
+            app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
